1b59ac6c9615f2c207f2983043af29e34736a70a
rdzdev <111528285+rdzdev@users.noreply.github.com>
2022-11-17 01:24:36 +0800

    Add files via upload

---
+++ testupdate.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Updater Script
+# By Ridz
+#-----------------------------
+red='\e[1;31m'
+green='\e[0;32m'
+purple='\e[0;35m'
+orange='\e[0;33m'
+NC='\e[0m'
+echo ""
+echo -e "[ ${green}INFO${NC} ] Autoscript Update Begin . . ."
+sleep 1
+echo ""
+#echo -e "[ ${green}INFO${NC} ] Update Autoscript
+#
+#✅ Update BUG Maxis
+#"
+echo -e "[ ${green}INFO${NC} ] Update Autoscript Details :
+
+✅ Update Bug
+"
+sleep 3
+
+cd /usr/bin
+#rm menu
+rm xraay
+cd /usr/bin
+wget -O xraay "https://raw.githubusercontent.com/rdzdev/rdzdevmulti/main/menu/xraay.sh" && chmod +x xraay
+#cd
+cd
+
+echo -e "[${green}INFO${NC}] Successfully Install New Update File !"
+sleep 1
